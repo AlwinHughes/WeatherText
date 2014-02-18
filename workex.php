a6624a07911626b8ca37feea93427d45dcf73776
AlwinHughes <alwin.hughes@btopenworld.com>
2014-02-18 15:42:42 +0000

    comments and calling
    added comments and other bits of the program that will be needed.

--- workex.php
+++ workex.php
@@ -1,18 +1,26 @@
 <?php
-  
+  //getting body and number 
 $body = $_REQUEST['Body'];
 $num = $_REQUEST['From'];
   
+  
+  
+  
+  //obtaining json
   function weather_forcast ($town, $country) {
     $jsonurl = "http://api.openweathermap.org/data/2.5/weather?q=".$town.",".$country;
     $json = file_get_contents($jsonurl);
     $weather = json_decode($json,true);
     $des = $weather [weather][0][description]
-  
+    
+    
+    //Twilio api bit
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
     echo "<Response>\n";
     echo "<Sms>The weather in ".$town." is currently ".$Des."<Sms> \n";
     echo "</Response>\n";
   }
+  
+  weather_forcast(body[0],body[1]);
   
 ?>
