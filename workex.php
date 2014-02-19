85f5a4281c3f35a9ce354606ccf28cf55dcd423d
AlwinHughes <AlwinHughes@users.noreply.github.com>
2014-02-19 14:24:07 +0000

    fixed explode

--- workex.php
+++ workex.php
@@ -3,7 +3,7 @@
 $body = $_REQUEST['Body'];
 
 
-$bodyarray = explode($body,' ');
+$bodyarray = explode(' ', $body);
 $num = $_REQUEST['From'];
 $unusabblechar = array("1","2","3","4","5","6","7","8","9","0")
   
